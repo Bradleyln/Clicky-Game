6cbd7e8f2e2abba8202024ca040cf432a8ad137c
unknown <bradleyln293@gmail.com>
2019-06-28 12:01:58 -0400

    add images

---
+++ src/images.js
@@ -0,0 +1,29 @@
+import hero1 from "./images/ant.jpg";
+import hero2 from "./images/captain.jpg";
+import hero3 from "./images/fury.jpg";
+import hero4 from "./images/hulk.jpg";
+import hero5 from "./images/ironman.jpg";
+import hero6 from "./images/marvel.jpg";
+import hero7 from "./images/panther.jpg";
+import hero8 from "./images/shuri.jpg";
+import hero9 from "./images/spider.png";
+import hero10 from "./images/strange.jpg";
+import hero11 from "./images/suit.jpg";
+import hero12 from "./images/thanos.jpg";
+
+const images = [
+  hero1,
+  hero2,
+  hero3,
+  hero4,
+  hero5,
+  hero6,
+  hero7,
+  hero8,
+  hero9,
+  hero10,
+  hero11,
+  hero12
+];
+
+export default images;
